639c79f8dda2557d9bda560d8b43abb201bc144b
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-04 13:36:36 +0530

    Initial Commit

---
+++ VoucherUp/VoucherUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoucherUpApp.swift
+//  VoucherUp
+//
+//  Created by Bhumika Patel on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct VoucherUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
