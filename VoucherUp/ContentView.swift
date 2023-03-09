2eddba8b72221f467756d51ead5dd34dcbee1907
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-09 10:22:13 +0530

    tab background color set

--- VoucherUp/ContentView.swift
+++ VoucherUp/ContentView.swift
@@ -8,98 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var email = "bhumi@test.com"
-    @State private var password = "Abc@123"
+    
     var body: some View {
-        VStack {
-            GeometryReader { geometry in
-                Image("BI1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                   // .offset(y: geometry.frame(in: .global).midY/50)
-                   // .padding(.bottom)
-                    .edgesIgnoringSafeArea(.all)
-                
-            }
-            VStack{
-                VStack(alignment: .center, spacing: 1.0){
-                    VStack(alignment: .center, spacing: 10){
-                        Text("Welcome Back.").bold()
-                            .font(.system(size: 26))
-                            .foregroundColor(Color("Main2Color"))
-                        
-                        Text("Log in to get started.")
-                            .foregroundColor(Color("Main2Color"))
-                           
-                           // .padding(.leading)
-                        Divider()
-                            
-                            .padding(.leading)
-                        // divider with short to with and height set
-                            .frame(width: 250, height: 1)
-                        // divider color set
-                            .overlay(Color("Main2Color"))
-                            
+        TabView {
+           FirstView()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
                     }
-                    .padding(.vertical)
-                   
-                    VStack(alignment: .leading, spacing: 20){
-                        UITextField(value: $email, placeholder: "email", icon: "envelope.fill")
-                            .keyboardType(.emailAddress)
-                            .autocapitalization(.none)
-                            .padding()
-                           
-                        UITextField(value: $password, placeholder: "password", isSecure: true, icon: "lock.fill")
-                            .autocapitalization(.none)
-                            .padding()
-                            .padding(.vertical,-30)
+                    .toolbar(.visible, for: .tabBar)
+
+                    .toolbarBackground(Color.yellow, for: .tabBar)
+
+                Text("Search")
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
                     }
-                  //  .padding()
-                    .padding(.vertical,25)
-                    VStack(alignment: .leading, spacing: 20){
-                        Button(action: {}, label: {
-                            Text("Log In").bold()
-                                .frame(width: 330, height: 45)
-                                .foregroundColor(Color("MainColor"))
-                                .background(
-                                    Rectangle()
-                                        .fill(Color("Primary"))
-                                        .cornerRadius(15)
-                                        .shadow(
-                                            color: Color.gray.opacity(0.7),
-                                            radius: 8,
-                                            x: 0,
-                                            y: 8
-                                        )
-                                    )
-                        })
+                    .toolbar(.visible, for: .tabBar)
+
+                    .toolbarBackground(Color.yellow, for: .tabBar)
+
+                Text("Notification")
+                    .tabItem {
+                        Label("Notification", systemImage: "bell")
                     }
-                    .padding()
-                    VStack(spacing: 20){
-                        HStack{
-                            Text("Not a member?")
-                              
-                            Button(action: {}, label: {
-                                Text("Sign up now")
-                            })
-                        }
-                        .foregroundColor(Color("LinkColor"))
+                    .toolbar(.visible, for: .tabBar)
+
+                    .toolbarBackground(Color.yellow, for: .tabBar)
+
+                Text("Settings")
+                    .tabItem {
+                        Label("Settings", systemImage: "gearshape")
                     }
-                    .padding()
-                    VStack(spacing: 20){
-                        Button(action: {}, label: {
-                            Text("Forgot your password?")
-                        })
-                        .foregroundColor(Color("LinkColor"))
-                    }
-                }
-            }
-            .padding()
-            .background(Color("MainColor"))
-            .clipShape(Rounded())
-            .padding(.top, -100)
+                    .toolbar(.visible, for: .tabBar)
+
+                    .toolbarBackground(Color("Primary"), for: .tabBar)
         }
+       // .toolbarBackground(.red, for: .tabBar)
+//        .onAppear() {
+//            UITabBar.appearance().backgroundColor = UIColor(Color("Primary"))
+//            }
+        .accentColor(Color("MainColor"))
        
     }
 }
