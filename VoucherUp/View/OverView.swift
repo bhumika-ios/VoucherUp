c8b26fc9d6c6abc6aeff8504ecfc4e9bec3554cf
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-09 16:25:52 +0530

    create overview doe details for category

---
+++ VoucherUp/View/OverView.swift
@@ -0,0 +1,46 @@
+//
+//  OverView.swift
+//  VoucherUp
+//
+//  Created by Bhumika Patel on 09/03/23.
+//
+
+import SwiftUI
+
+struct OverView: View {
+    let detailModel: DetailModel
+    var body: some View {
+        VStack{
+            VStack(alignment: .leading){
+                Text("About").bold()
+                Text(detailModel.about)
+            }
+            .padding()
+            VStack(alignment: .leading){
+                Text("Opening Hours").bold()
+                HStack{
+                    Text("Monday")
+                    Spacer()
+                    Text("11:00 - 22:00")
+                }
+                HStack{
+                    Text("Tuesday")
+                    Spacer()
+                    Text("11:00 - 22:00")
+                }
+                HStack{
+                    Text("Wednesday")
+                    Spacer()
+                    Text("11:00 - 22:00")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct OverView_Previews: PreviewProvider {
+    static var previews: some View {
+        OverView(detailModel: detailModel[0])
+    }
+}
